f175830ad16496fd69c40345f41034e058b9af71
zcy <congyao.zheng@datadoghq.com>
2024-11-05 13:51:31 +0100

    Add beforesend and dictionary download

--- packages/rum-core/src/domain/assembly.ts
+++ packages/rum-core/src/domain/assembly.ts
@@ -12,6 +12,7 @@ import {
   isExperimentalFeatureEnabled,
   ExperimentalFeature,
   getConnectivity,
+  addTelemetryDebug,
 } from '@datadog/browser-core'
 import type { RumEventDomainContext } from '../domainContext.types'
 import type {
@@ -213,6 +214,29 @@ function shouldSend(
   domainContext: RumEventDomainContext,
   eventRateLimiters: { [key in RumEventType]?: EventRateLimiter }
 ) {
+  if (event.type === RumEventType.ACTION) {
+    const wordsRegex = /(\w+)/
+    if (event.action.name && window.DD_WHITELIST_DICTIONARY) {
+      // substitute the matched patterns with sanitized words
+      let masked = false
+      event.action.target?.name.replace(wordsRegex, (word: string) => {
+        const normalized = word.toLowerCase()
+        if (window.DD_WHITELIST_DICTIONARY[normalized]) {
+          return word
+        }
+        masked = true
+        return 'MASKED'
+      })
+      // eslint-disable-next-line no-underscore-dangle
+      if (masked && event._dd.action) {
+        // eslint-disable-next-line no-underscore-dangle
+        event._dd.action.name_source = 'mask_unverified'
+      }
+    } else if (!window.DD_WHITELIST_DICTIONARY) {
+      // add metrics when there is no dictionary
+      addTelemetryDebug('Action names dictionary not loaded')
+    }
+  }
   if (beforeSend) {
     const result = limitModification(event, modifiableFieldPathsByEvent[event.type], (event) =>
       beforeSend(event, domainContext)
